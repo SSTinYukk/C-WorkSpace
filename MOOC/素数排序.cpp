b8c2f821176d4ef50a62674b6f4977fe4d39146a
SSTinYukk <13999730523@163.com>
2020-12-22 18:43:17 +0800

    MOOC

--- MOOC/素数排序.cpp
+++ MOOC/素数排序.cpp
@@ -1,31 +1,40 @@
 #include<stdio.h>
 #include<math.h>
-bool isprime(int n)
+int isprime(int n)
 {
     int i;
+    if(n==1)
+    return 0;
+    if(n==2){
+        return 1;
+    }
     for(i=2;i<=sqrt(n);i++){
         if(n%i==0){
-            return false;
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
 int main()
 {
     int n=10,is=0,a[100],i;
-    for(i=0;i<10;i++){
-        scanf("%d",a[i]);
+    for(i=0;i<n;i++){
+        scanf("%d,",&a[i]);
     }
-    for(i=0;i<10;i++)
+    int t=0;
+    for(i=0;i<n;i++)
     {
         if(!isprime(a[i])){
             a[i]=0;
-            n--;    
+            t++;
+        }else{
+            is=1;
         }
     }
+    
     int j;
     for(i=0;i<9;i++){
-        for(j=0;i<9-i;j++){
+        for(j=0;j<9-i;j++){
             if(a[j]>a[j+1]){
                 int t=a[j];
                 a[j]=a[j+1];
@@ -33,8 +42,19 @@ int main()
             }
         }
     }
-    for(i=0;i<n;i++){
-        printf("%d",a[i]);
-    }
+    // for(i=0;i<n;i++){
+    //     printf("%d ",a[i]);
+    // }
+    // printf("\n");
+
+        for(i=t;i<n;i++){
+            printf("%d",a[i]);
+            if(i!=n-1){
+                printf(",");
+            }
+        }
+        if(is==0){
+            printf("Not found");
+        }
     return 0;
 }
