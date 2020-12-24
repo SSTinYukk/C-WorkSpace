b03b668c33cbae7d6e9ee7cd61eb9b626ed2740d
SSTinYukk <13999730523@163.com>
2020-12-24 15:05:33 +0800

    PTA

--- PTA/6-3 递归求Fabonacci数列 (20分).cpp
+++ PTA/6-3 递归求Fabonacci数列 (20分).cpp
@@ -14,5 +14,12 @@ int main()
 
 /* 你的代码将被嵌在这里 */
 int f(int n){
-    if()
+    if(n<=0){
+        return 0;
+    }
+    if(n<=2){
+        return 1;
+    }else{
+        return f(n-1)+f(n-2);
+    }
 }
