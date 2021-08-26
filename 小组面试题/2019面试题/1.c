6dd56201885e13bed30311aaec04aebc11f488b3
SSTinYukk <13999730523@163.com>
2021-08-26 21:39:21 +0800

    LINUX

--- 小组面试题/2019面试题/1.c
+++ 小组面试题/2019面试题/1.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-int main(int argc, char *argv[]) 
-{ 
-    int val = 2018; 
-    int *pi = 2019; 
-    pi = &val; 
-    *pi = 0; 
-    return 0; 
+int main(int argc, char *argv[]) {
+  int val = 2018;
+  int *pi = 2019;
+  pi = &val;
+  *pi = 0;
+  return 0;
 }
